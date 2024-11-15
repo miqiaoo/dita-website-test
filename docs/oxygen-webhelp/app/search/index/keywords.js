3dfdd8ebe8edb190165331070f04e7ebe918e1d6
miqiaoo <miqiao1.xie@protonmail.com>
2024-11-15 10:44:01 +0800

    add dita source and webhelp

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,14 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Airplay\u6295\u5C4F\u6B65\u9AA4",p:["p1"]},{w:"\u5982\u4F55\u4F7F\u7528HDMI\u6295\u5C4F",p:["p2"]},{w:"\u5982\u4F55\u4F7F\u7528Airplay\u5C06iPad\u6295\u5C4F\u81F3\u6295\u5F71\u4EEA",p:["p3"]},{w:"AirPlay\u53C2\u6570\u4FE1\u606F",p:["p4"]},{w:"\u4E0D\u80FD\u4F7F\u7528AirPlay\u6295\u5C4F",p:["p5"]},{w:"Airplay",p:["p6"]},{w:"\u4EC0\u4E48\u662FHDMI",p:["p7"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3];
+ph["p2"]=[4];
+ph["p3"]=[5];
+ph["p4"]=[6];
+ph["p5"]=[7];
+ph["p6"]=[8];
+ph["p7"]=[9];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {7:2,4:1,2:-1,6:1,1:-1};
